029d3b30dc5f16a282fd52a32eae10ddb239ef87
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-03-05 21:12:25 +0200

    Junk Navigation Way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  RECIPES_MENU_ITEM: number = 1;
+  SHOPPING_LIST_MENU_ITEM: number = 2;
+
+  selectedMenuItem: number = this.RECIPES_MENU_ITEM;
+
+  onNavigatedToRecipes() {
+    this.selectedMenuItem = this.RECIPES_MENU_ITEM;
+  }
+
+  onNavigatedToShoppingList() {
+    this.selectedMenuItem = this.SHOPPING_LIST_MENU_ITEM;
+  }
 }

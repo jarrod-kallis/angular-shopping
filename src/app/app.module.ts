ab43c624026a82d30349fa4d272972f43f2f8139
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-12-31 13:03:21 +0200

    Redux Dev Tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { CustomFormsModule } from "ng2-validation";
 import { HttpClientModule } from "@angular/common/http";
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -12,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
 import { ProvidersModule } from './providers.module';
 import { appReducer } from './store/app.reducer';
 import { AuthenticationEffects } from './authentication/store/authentication.effects';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { AuthenticationEffects } from './authentication/store/authentication.eff
     SharedModule,
     ProvidersModule,
     StoreModule.forRoot(appReducer),
-    EffectsModule.forRoot([AuthenticationEffects])
+    EffectsModule.forRoot([AuthenticationEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
